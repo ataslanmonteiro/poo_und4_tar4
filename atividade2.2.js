c1d7ba0d0b79f324465848a93a648f39029a9caf
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2025-01-12 17:53:25 -0300

    atividade2.2 teste 1

--- atividade2.2.js
+++ atividade2.2.js
@@ -1,6 +1,13 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function contarUns(string) {
   return (string.match(/1/g) || []).length;
 }
 
-let string = prompt("Digite uma string de 0s e 1s: ");
-console.log(`A string '${string}' tem ${contarUns(string)} caracteres '1'.`);
+readline.question("Digite uma string de 0s e 1s: ", (string) => {
+  console.log(`A string '${string}' tem ${contarUns(string)} caracteres '1'.`);
+  readline.close();
+});
