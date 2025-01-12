7ab0073672ce5e98ebd26c349a88392ad8a44bcb
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2025-01-12 16:46:16 -0300

    atividade1 teste 1

---
+++ atividade1.js
@@ -0,0 +1,56 @@
+class Usuario {
+  constructor(RG, CPF, nome) {
+    this.RG = RG || null;
+    this.CPF = CPF || null;
+    this.nome = nome || "";
+  }
+  getRG() {
+    return this.RG;
+  }
+  getRG(RG) {
+    this.RG = RG;
+  }
+  getCPF() {
+    return this.CPF;
+  }
+  getCPF(CPF) {
+    this.CPF = CPF;
+  }
+  getNome() {
+    return this.nome;
+  }
+  getNome(nome) {
+    this.nome = nome;
+  }
+}
+class Conta {
+  constructor(agencia, usuario, saldo) {
+    this.agencia = agencia;
+    this.usuario = usuario;
+    this.saldo = saldo;
+  }
+  getAgencia() {
+    return this.agencia;
+  }
+  getUsuario() {
+    return this.usuario;
+  }
+  getSaldo() {
+    return this.saldo;
+  }
+}
+let usuario = new Usuario();
+
+usuario.setRG(01)
+usuario.setCPF(123.456.789-78);
+usuario.setNome("João da Silva");
+
+let conta = new Conta(1234, usuario, 1000.98);
+
+console.log("Dados da Conta: ");
+console.log("Agência: ", conta.getAgencia());
+console.log("Saldo: ", conta.getSaldo());
+console.log("Dados do Usuário: ");
+console.log("RG: ", conta.getUsuario().getRG());
+console.log("CPF: ", conta.getUsuario().getCPF());
+console.log("RG: ", conta.getUsuario().getNome());
