cf198ad80f4b695aefc634b01a9a4fba44a5b6fb
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2025-01-12 17:49:47 -0300

    atividade2.1 teste ok

--- atividade2.1.js
+++ atividade2.1.js
@@ -1,6 +1,15 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Digite um nome: ", (nome) => {
+  console.log(`O nome '${nome}' tem ${contarLetras(nome)} letras.`);
+  rl.close();
+});
+
 function contarLetras(nome) {
   return nome.length;
 }
-
-let nome = prompt("Digite um nome: ");
-console.log(`O nome '${nome}' tem ${contarLetras(nome)} letras.`);
